db5fc768020e4787cbb6245c248a6e5e716a687e
chamberezigbo <chamberzigbo@gmail.com>
2021-03-18 12:33:34 +0100

    done with ui

---
+++ index.js
@@ -0,0 +1,72 @@
+let myQuestions = {
+
+	question1: {
+		num: 1,
+		questionTitle:"What is my name?",
+
+		questionOptions: {
+			opt1: "Cindy",
+			opt2: "Candy",
+			opt3: "Adaure",
+			opt4:"Baby"
+		},
+		correct: this.questionOptions.opt3
+	},
+       question2: {
+		num: 2,
+		questionTitle:" month are there in a year?",
+
+		questionOptions: {
+			opt1: 12,
+			opt2: 23,
+			opt3: 8,
+			opt4: 1
+		},
+		correct: this.questionOptions.opt1
+	},
+       question3: {
+		num: 3,
+		questionTitle:"What is the name od the founder of rad5 tech hub?",
+
+		questionOptions: {
+			opt1: "Condor and trust",
+			opt2: "Prince and Presh",
+			opt3: "Adaure",
+			opt4:"Baby"
+		},
+		correct: this.questionOptions.opt3
+	},
+       question4: {
+		num: 4,
+		questionTitle:"What is my name?",
+
+		questionOptions: {
+			opt1: "Cindy",
+			opt2: "Candy",
+			opt3: "Adaure",
+			opt4:"Baby"
+		},
+		correct: this.questionOptions.opt3
+	},
+       question5: {
+		num: 5,
+		questionTitle:"What is my name?",
+
+		questionOptions: {
+			opt1: "Cindy",
+			opt2: "Candy",
+			opt3: "Adaure",
+			opt4:"Baby"
+		},
+		correct: this.questionOptions.opt3
+	},
+
+
+}
+
+let question = document.getElementById('question-container')
+
+
+function Agreed() {
+	
+}
